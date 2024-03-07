6242d0e307bb662a99b6c3bc3b7cd1832c4b4739
En Rong <53928333+chownces@users.noreply.github.com>
2024-03-07 17:11:45 +0800

    Implement scraping whitelist

--- web_scraper/main.py
+++ web_scraper/main.py
@@ -1,11 +1,13 @@
 from ecaption_utils.kafka.faust import (
     get_faust_app,
     initialize_topics,
+    get_error_handler,
     FaustApplication,
 )
 from ecaption_utils.kafka.topics import Topic, get_event_type
-from utils import extract_article_url
+from utils import extract_article_url, check_whitelist, is_https
 import os
+from urllib.parse import urlparse, urljoin
 
 broker_url = os.environ.get("KAFKA_BROKER_URL")
 port = os.environ.get("WEB_SCRAPER_SERVICE_PORT")
@@ -13,17 +15,33 @@ port = os.environ.get("WEB_SCRAPER_SERVICE_PORT")
 app = get_faust_app(FaustApplication.WebScraper, broker_url=broker_url, port=port)
 topics = initialize_topics(
     app,
-    [
-        Topic.NEW_ARTICLE_URL,
-        Topic.NEW_ARTICLE_EXTRACTED,
-    ],
+    [Topic.NEW_ARTICLE_URL, Topic.NEW_ARTICLE_EXTRACTED],
 )
+handle_error = get_error_handler(app)
 
 
 @app.agent(topics[Topic.NEW_ARTICLE_URL])
 async def handle_new_article(event_stream):
     async for event in event_stream:
-        extracted = extract_article_url(event.url)
+        parsed_url = urlparse(event.url)
+
+        if not is_https(parsed_url):
+            await handle_error(
+                event.request_id,
+                error_type=FaustApplication.WebScraper,
+                error_message=f"URL is not HTTPs! {event.url}",
+            )
+            continue
+        if not check_whitelist(parsed_url):
+            await handle_error(
+                event.request_id,
+                error_type=FaustApplication.WebScraper,
+                error_message=f"URL not whitelisted! {event.url}",
+            )
+            continue
+
+        stripped_url = urljoin(event.url, parsed_url.path)
+        extracted = extract_article_url(stripped_url)
 
         publish_to = Topic.NEW_ARTICLE_EXTRACTED
         Event = get_event_type(publish_to)
--- web_scraper/utils.py
+++ web_scraper/utils.py
@@ -1,4 +1,17 @@
 from trafilatura import fetch_url, bare_extraction
+from urllib.parse import ParseResult
+
+whitelisted_domains: set[str] = set()
+with open(".whitelist", "rb") as file:
+    whitelisted_domains = set([url.strip().decode() for url in file])
+
+
+def is_https(parsed_url: ParseResult) -> bool:
+    return parsed_url.scheme == "https"
+
+
+def check_whitelist(parsed_url: ParseResult) -> bool:
+    return parsed_url.hostname in whitelisted_domains
 
 
 def extract_article_url(url: str) -> dict[str, str]:
